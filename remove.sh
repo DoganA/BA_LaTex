89232222340f20372203a02cfc8ed0d21187c662
Dogan <d.alk@hotmail.de>
2018-11-09 14:15:37 +0100

    Listingsverzeichnis bearbeitet

--- remove.sh
+++ remove.sh
@@ -1,1 +1,1 @@
-rm -rf  --no-preserve-root *.log *.lof *.lot *.bcf *.toc *.run.xml *.lol *.blg *.bbl *.out *.aux *.swp *.synctex
+rm -rf  --no-preserve-root *.log *.lof *.lot *.bcf *.toc *.run.xml *.lol *.blg *.bbl *.out *.aux *.swp *.synctex *.tps
